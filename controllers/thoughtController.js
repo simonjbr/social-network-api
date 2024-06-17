eb8617727e978151bb46d9b3b047f1f43a61730c
Simon Brown <simonandon@live.com.au>
2024-06-17 10:02:34 +0800

    Created function and route for get thought by id

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -18,6 +18,27 @@ const getThoughts = async (req, res) => {
 	}
 };
 
+// GET thought by id /api/thoughts/:thoughtId
+const getThoughtById = async (req, res) => {
+	try {
+		const thoughtId = req.params.thoughtId;
+
+		const thought = await Thought.findById(thoughtId);
+		if (!thought) {
+			res.status(404).json({
+				message: `Could not find thought with id: ${thoughtId}`,
+			});
+			return;
+		}
+
+		res.status(200).json(thought);
+	} catch (error) {
+		console.log(`Error at GET /api/thoughts/:thoughtId`, error);
+		res.status(500).json(error);
+	}
+};
+
 module.exports = {
 	getThoughts,
+	getThoughtById,
 };
--- routes/api/thoughts/index.js
+++ routes/api/thoughts/index.js
@@ -1,4 +1,11 @@
 const router = require('express').Router();
-const { getThoughts } = require('../../../controllers/thoughtController');
+const { getThoughts, getThoughtById, } = require('../../../controllers/thoughtController');
+
+// /api/thoughts GET all thoughts
+router.route('/').get(getThoughts);
+
+// /api/thoughts/:thoughtId GET thought by id
+router.route('/:thoughtId').get(getThoughtById);
+
 
 module.exports = router;
